fdee7b4c97eeb9aa2789543d7b15ea32a8d6ed0c
Sebastian Staudt <koraktor@gmail.com>
2010-11-12 15:26:13 +0100

    Added a CLI command to add files to a repository
    The repository is loaded from config files in either the current directory,
    the user's home or from the global settings directory (i.e. /etc on Unix).

--- lib/silo/cli.rb
+++ lib/silo/cli.rb
@@ -15,11 +15,25 @@ module Silo
   # @since 0.1.0
   class CLI < Rubikon::Application::Base
 
+    set :config_file, '.silo'
+    set :config_format, :ini
     set :help_banner, 'Usage: silo'
+
+    pre_execute do
+      @repo_path = config['repository']['path']
+    end
 
     default do
       puts "This is Silo. A Git-based backup utility.\n\n"
       call :help
+    end
+
+    option :prefix, [:path]
+    command :add, -1, 'Store one or more files in the repository' do
+      repo = Repository.new @repo_path
+      args.each do |file|
+        repo.add file, prefix.path
+      end
     end
 
     command :init, 0..1, 'Initialize a Silo repository' do
