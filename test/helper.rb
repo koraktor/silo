ef26a54bad3453dc911136599705cbe7a464d4ad
Sebastian Staudt <koraktor@gmail.com>
2011-09-20 12:50:12 +0200

    Fixed test helper

--- test/helper.rb
+++ test/helper.rb
@@ -1,11 +1,14 @@
 # This code is free software; you can redistribute it and/or modify it under
 # the terms of the new BSD License.
 #
-# Copyright (c) 2010, Sebastian Staudt
+# Copyright (c) 2010-2011, Sebastian Staudt
+
+require 'rubygems'
+require 'test/unit'
+
+require 'shoulda'
 
 $: << File.join(File.dirname(__FILE__), '..', 'lib')
 $: << File.dirname(__FILE__)
 require 'silo'
 include Silo
-
-Bundler.require :test
